f685ed04f4ce044d13b3e550f0d051d5abaf3765
lord20091 <abdulazizbozorov009@gmail.com>
2024-04-24 15:20:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let letterSearch = ['non', 'asal', 'banan', 'olma', 'aziza', 'Gul', 'mashina',]
+
+let searchSoz = alert(`tasdiq?`)
+
+natijaTrue = []
+
+for (const narsa of letterSearch) {
+    if(narsa.includes(`n`)) {
+        natijaTrue.push(narsa)
+    } 
+}
+
+alert(natijaTrue)
